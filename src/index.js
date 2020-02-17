fbcdf0c521f60c2611932d8c751131783f8bb09b
romanitwork <workromanmail@gmail.com>
2020-02-17 17:19:55 +0200

    fixed with fragment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import './styles.css';
 import fetchCountries from './fetch/fetchCountries';
 import template from './templates/template.hbs';
 import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
+import '../node_modules/pnotify/src/PNotifyBrightTheme.css';
 
 const debounce = require('lodash.debounce');
+const fragment = document.createDocumentFragment();
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
@@ -21,23 +23,25 @@ const makeMarkup = debounce(function input() {
   }
   fetchCountries(searchValue)
     .then(data => {
-      if (data.length > 5) {
+      if (data.length > 10) {
         PNotify.error({
           text: 'Too many matches found. Please enter a more specisic query!',
         });
         clearOutput();
-      } else if (data.length > 1 && data.length < 5) {
-        data.map(country => {
-          refs.outputList.insertAdjacentHTML(
+      } else if (data.length > 2 && data.length <= 10) {
+        data.forEach(country => {
+          const newLi = document.createElement("li");
+          newLi.insertAdjacentHTML(
             'beforeend',
-            `<li>${country.name}</li>`,
+            `${country.name}`,
           );
+          fragment.appendChild(newLi);
         });
+        refs.outputList.appendChild(fragment);
         refs.output.innerHTML = '';
         PNotify.closeAll();
       } else if (data.length === 1) {
         const finalTemplate = template(data);
-        console.log(finalTemplate)
         refs.output.insertAdjacentHTML('beforeend', finalTemplate);
         refs.outputList.innerHTML = '';
         PNotify.closeAll();
@@ -50,5 +54,4 @@ refs.searchForm.addEventListener('input', makeMarkup);
 
 function clearOutput() {
   refs.outputList.innerHTML = '';
-  refs.output.innerHTML = '';
 }
