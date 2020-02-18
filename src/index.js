333a13fa9b6694b59e2c5c7e643764047e009378
romanitwork <workromanmail@gmail.com>
2020-02-18 09:07:06 +0200

    buttons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import './styles.css';
 import fetchCountries from './fetch/fetchCountries';
 import template from './templates/template.hbs';
 import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
+import PNotifyButtons from '../node_modules/pnotify/dist/es/PNotifyButtons.js';
 import '../node_modules/pnotify/src/PNotifyBrightTheme.css';
+
 
 const debounce = require('lodash.debounce');
 const fragment = document.createDocumentFragment();
